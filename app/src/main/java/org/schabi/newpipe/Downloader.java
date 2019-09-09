774bc80968c823947288859b5efa0ef3373f5aac
frantisespurny <frantisek.spurny1@gmail.com>
2019-09-09 15:44:19 +0200

    url masking

--- app/src/main/java/org/schabi/newpipe/Downloader.java
+++ app/src/main/java/org/schabi/newpipe/Downloader.java
@@ -3,6 +3,9 @@ package org.schabi.newpipe;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
+import com.smartlook.sdk.smartlook.interceptors.SmartlookOkHttpInterceptor;
+import com.smartlook.sdk.smartlook.interceptors.UrlMask;
+
 import org.schabi.newpipe.extractor.exceptions.ReCaptchaException;
 import org.schabi.newpipe.extractor.utils.Localization;
 
@@ -49,6 +52,7 @@ public class Downloader implements org.schabi.newpipe.extractor.Downloader {
     private Downloader(OkHttpClient.Builder builder) {
         this.client = builder
                 .readTimeout(30, TimeUnit.SECONDS)
+                .addNetworkInterceptor(new SmartlookOkHttpInterceptor(new UrlMask("sqp=.*")))
                 //.cache(new Cache(new File(context.getExternalCacheDir(), "okhttp"), 16 * 1024 * 1024))
                 .build();
     }
