e83afcb2b60be73ee259d45d5d1a30af6bfffc5d
frantisespurny <frantisek.spurny1@gmail.com>
2019-09-17 11:07:25 +0200

    SmartlookOkHttpInterceptor and UrlMask private for now

--- app/src/main/java/org/schabi/newpipe/Downloader.java
+++ app/src/main/java/org/schabi/newpipe/Downloader.java
@@ -3,9 +3,6 @@ package org.schabi.newpipe;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
 
-import com.smartlook.sdk.smartlook.Smartlook;
-import com.smartlook.sdk.smartlook.interceptors.SmartlookOkHttpInterceptor;
-
 import org.schabi.newpipe.extractor.exceptions.ReCaptchaException;
 import org.schabi.newpipe.extractor.utils.Localization;
 
@@ -52,7 +49,6 @@ public class Downloader implements org.schabi.newpipe.extractor.Downloader {
     private Downloader(OkHttpClient.Builder builder) {
         this.client = builder
                 .readTimeout(30, TimeUnit.SECONDS)
-                .addNetworkInterceptor(new SmartlookOkHttpInterceptor())
                 //.cache(new Cache(new File(context.getExternalCacheDir(), "okhttp"), 16 * 1024 * 1024))
                 .build();
     }
